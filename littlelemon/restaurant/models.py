68eb8fd750a90fbf95d8d82f2509261a75744ac4
offmolly <quickpyroofficial@gmail.com>
2024-05-08 01:55:42 +0530

    Added tests for Menu model and Menu view

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,8 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.IntegerField()
 
-    def __str__(self) -> str:
-        return f'{self.title}'
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Menu
+from .serializers import MenuSerializer
 
-# Create your tests here.
+
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream",price=80,inventory=10)
+        self.assertEqual(str(item),"IceCream : 80")
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.item = Menu.objects.create(id=1,title="IceCream",price=80.00,inventory=20)
+        self.test_data = [
+            {'id': 1,'title':"IceCream",'price':'80.00','inventory':20}
+        ]
+    
+    def test_getall(self):
+        menus = Menu.objects.all()
+        serializer = MenuSerializer(menus,many=True)
+        serialized_data = serializer.data
+        self.assertEqual(serialized_data,self.test_data)
