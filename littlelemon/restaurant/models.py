38e66b4359869452d9205f1a56e26b215b17bd8d
offmolly <quickpyroofficial@gmail.com>
2024-05-07 18:18:22 +0530

    added str dunder methods to the model

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,7 +6,13 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f'{self.title}'
+
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.SmallIntegerField()
     bookingdate = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.name}'
