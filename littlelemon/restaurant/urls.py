744313d3a10441d1fc4cc87cb1e15e9f09042348
offmolly <quickpyroofficial@gmail.com>
2024-05-08 16:00:30 +0530

    updated template for index.html

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='home'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
